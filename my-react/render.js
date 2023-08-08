f4c5a94b2112d5c8ecc45750605468e029fe35c7
guohui <2806815594@qq.com>
2023-08-08 10:16:40 +0800

    feat: fiber节点

--- my-react/render.js
+++ my-react/render.js
@@ -1,5 +1,5 @@
-function render(element, container) {
-  const {type, props} = element
+function createDom(fiber) { 
+  const {type, props} = fiber
   const dom = type == 'TEXT_ELEMENT'
     ? document.createTextNode('')
     : document.createElement(type)
@@ -8,10 +8,75 @@ function render(element, container) {
   Object.keys(props).filter(key => key != 'children').forEach(name => dom[name] = props[name])
 
   // 递归render每个节点
-  props.children.forEach(child => render(child, dom))
-  // console.log(element);
-  container.appendChild(dom)
+  // props.children.forEach(child => render(child, dom))
+
+  // container.appendChild(dom)
+  return dom
+}
+function render(element, container) {
+  nextUnitOfwork = {
+    dom: container,
+    props: {
+      children: [element]
+    }
+  }
 }
 
 
+let nextUnitOfwork = null
+function workLoop(deadline) {
+  let shouleYield = false
+  while(nextUnitOfwork && !shouleYield) {
+    console.log(shouleYield);
+    nextUnitOfwork = performUnitOfWork(nextUnitOfwork)
+    shouleYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(fiber) {
+  if(!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+
+  if(fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+
+  const elements = fiber.props.children
+  debugger
+  let index = 0
+  let prevSibling = null
+
+  while(index < elements.length) {
+    const element = elements[index]
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }
+    if(index == 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+    prevSibling = newFiber
+    index++
+  }
+
+  if(fiber.child) {
+    return fiber.child
+  }
+  let nextFiber = fiber
+  while(nextFiber) {
+    if(nextFiber.sibling) {
+        return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
+
+}
 export default render
